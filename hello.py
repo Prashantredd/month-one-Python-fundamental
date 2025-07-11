d642c60a72f5e24d92e50a7b1e770de599c008e4
prashant <gajjalaprashantreddy@gmail.com>
2025-07-11 19:24:08 +0530

    updated hello.py with new line

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
-print("hello world")
-print(2+4)
-print(9-8)
+print("hello world") # this the sample 
+# this variable type 
+name="Prashant"   # string
+age=22 # integer
+marks=86.5 # float 
+is_student=True # boolean
+print(type(name))
+print(type(age))
+print(type(marks))
+print(type(is_student))
