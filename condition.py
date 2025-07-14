fc8347c5b696cb86a01da8d768e3dd557c49c4b0
prashant <gajjalaprashantreddy@gmail.com>
2025-07-14 23:14:26 +0530

    add hello.py and condition.py files

---
+++ condition.py
@@ -0,0 +1,14 @@
+# if statement
+a=10
+if a==10:
+    print(f" a is {a}")
+else:
+    print(f" a is not equal to 10")    
+for i in range(1,a+1):# in sequence 
+    print(i) 
+b=1       
+while a>b: # here statement until break loop 
+    print(b)
+    b=b+1
+        
+    
