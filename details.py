240eb226284737758dab4cfc8d8d0d11e9f7ec45
prashant <gajjalaprashantreddy@gmail.com>
2025-08-07 23:09:30 +0530

    Updated details.py with new line

--- details.py
+++ details.py
@@ -1,4 +1,35 @@
-names      age     classs 
-G.prashant 22      b.tech
-g.dinesh   19      pharm.D
-G.Divya    19      b.tech 
+def add():
+    with open("contact.txt","a") as f:
+        name=input("enter name : ")
+        number=input("enter number : ")
+        gmail=input("enter the gmail : ")
+        f.write(f"{name},{number},{gmail}\n")
+def view() :       
+   with open("contact.txt","r") as f:
+     print(f.read())  
+
+def search():
+    p=input("enter number or name :").lower()
+    found=False
+    try :
+        with open("contact.txt","r") as f:
+            for i in f:
+                name,number,gmail=i.strip().split(",")
+                if p in name.lower() or p in number.lower():
+                    print(f"{name},{number},{gmail}")
+                    found=True
+                    break
+            if not found:
+                print("not found")    
+    except FileNotFoundError:
+        print("file not found error") 
+while True:            
+ choice=input(" enter the option :")
+ if choice=="add":
+    print(add())
+ elif choice=="view":
+    print(view())
+ elif choice=="search":
+    print(search())
+ elif choice=="exit":
+     break                                   
